905e5d7238c9b65e37233d7b626365a4c97865f1
Ralph <ralphedge@yahoo.com>
2009-04-15 12:53:09 -0500

    added checkboxes

--- javascripts/jquery.rest_in_place.js
+++ javascripts/jquery.rest_in_place.js
@@ -88,8 +88,6 @@ jQuery.fn.rest_in_place_select = function(url, objectName, attributeName) {
     
     var oldValue = e.html();
     
-    //e.html('<form action="javascript:void(0)" style="display:inline;"><textarea>'+oldValue+'</textarea></form>');
-    //e.find("select")[0].select();
     
     
     $(field).select();
@@ -113,7 +111,7 @@ jQuery.fn.rest_in_place_select = function(url, objectName, attributeName) {
             	var key = eval('(' + json + ')' )[objectName][attributeName] - 1;
             	var kvalue = $(field.children()[value=key]).text();
             	fvalue.html(kvalue);
-              //fvalue.html(eval('(' + json + ')' )[objectName][attributeName]);
+              
               $(c[1]).hide();
               $(c[0]).show();
               e.click(clickFunction);
@@ -126,6 +124,71 @@ jQuery.fn.rest_in_place_select = function(url, objectName, attributeName) {
   }
   this.click(clickFunction);
 }
+
+
+
+jQuery.fn.rest_in_place_checkbox = function(url, objectName, attributeName, truetext, falsetext) {
+  var e = this;
+
+	//wrap everything in a form tag
+	var oldValue = $(e).html();
+	$(e).html('<form action="javascript:void(0)" style="display:inline;">' + oldValue + '</form>');
+	
+	
+	var  c = $(this).children().children();
+	
+	var fvalue = $(c[0]);	
+  var field = $(c[1]);
+  
+  function clickFunction() {
+		//$(c[0]).hide();
+		
+		$(field).show();  
+    
+    var oldValue = e.html();
+    
+    
+    
+    $(field).select();
+    e.unbind('click');
+    $(field).blur(function(){
+      e.find("form").submit();
+      e.click(clickFunction);
+    })
+    e.find("form").submit(function(){
+      var value = $(field).attr("checked");
+      fvalue.html("Loading...");
+      jQuery.ajax({
+        "url" : url,
+        "type" : "post",
+        "data" : "_method=put&"+objectName+'['+attributeName+']='+encodeURIComponent(value)+(window.rails_authenticity_token ? "&authenticity_token="+window.rails_authenticity_token : ''),
+        "success" : function(){
+          jQuery.ajax({
+            "url" : url,
+            "beforeSend"  : function(xhr){ xhr.setRequestHeader("Accept", "application/javascript"); },
+            "success" : function(json){
+            	var result = eval('(' + json + ')' )[objectName][attributeName];
+            	
+            	if (result == true) fvalue.html(truetext);
+            	else fvalue.html(falsetext);
+            	
+            	//var kvalue = $(field.children()[value=key]).text();
+            	//fvalue.html(key);
+              
+              $(field).hide();
+              //$(c[0]).show();
+              e.click(clickFunction);
+            }
+          });
+        }
+      });
+      return false;
+    })
+  }
+  this.click(clickFunction);
+}
+
+
 
 
 
@@ -207,13 +270,55 @@ jQuery(function()
       }
     });
     
-    
-    
     url    = e.attr("url")       || url    || document.location.pathname;
     obj    = e.attr("object")    || obj;
     attrib = e.attr("attribute") || attrib;
     e.rest_in_place_select(url, obj, attrib);
   });
   
+
+	//checkbox code
+  
+  jQuery(".rest_in_place_checkbox").each(function()
+  {
+  	//set text for default checkboxes
+  	truetext = $(this).attr("true") || "Yes";
+  	falsetext = $(this).attr("false") || "No";
+  	  
+  	//need to drill down through the form tag  
+  	var c = $(this).children();
+  	$(this).prepend("<span>" + truetext + "</span>");
+
+  	box = $(c[0]);
+  	text = $(c[1]);
+  	  	
+  	$(box).hide();
+  	
+  	
+    var e = jQuery(this);
+    var url; var obj; var attrib;
+    e.parents().each(function()
+    {
+      url    = url    || jQuery(this).attr("url");
+      obj    = obj    || jQuery(this).attr("object");
+      attrib = attrib || jQuery(this).attr("attribute");
+    });
+    
+    e.parents().each(function()
+    {
+      if (res = this.id.match(/^(\w+)_(\d+)$/i)) 
+      {
+        obj = obj || res[1];
+      }
+    });
+    
+    url    = e.attr("url")       || url    || document.location.pathname;
+    obj    = e.attr("object")    || obj;
+    attrib = e.attr("attribute") || attrib;
+    e.rest_in_place_checkbox(url, obj, attrib, truetext, falsetext);
+  });
+	
+
+
   
 });
--- testapp/db/schema.rb
+++ testapp/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090412193856) do
+ActiveRecord::Schema.define(:version => 20090413055510) do
 
   create_table "roles", :force => true do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20090412193856) do
     t.datetime "updated_at"
     t.integer  "role_id"
     t.text     "about"
+    t.boolean  "approved"
   end
 
 end
